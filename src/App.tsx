9387d508b226c5005a1c88c5bf4ace6a8dd77cb1
ArkadiuszKolodynski <ArkadiuszKolodynski@users.noreply.github.com>
2024-07-26 15:16:25 +0200

    feat: setup MUI theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
+import { ThemeProvider } from "@material-tailwind/react";
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
+import reactLogo from "./assets/react.svg";
+import { theme } from "./theme";
+import viteLogo from "/vite.svg";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <>
+    <ThemeProvider value={theme}>
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -28,7 +30,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-    </>
+    </ThemeProvider>
   );
 }
 
